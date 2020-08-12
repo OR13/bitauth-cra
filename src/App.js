de1cf8c962538c196a43ead9241c99d1b0080ace
Orie Steele <orie@transmute.industries>
2020-08-12 15:24:15 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { instantiateSecp256k1 } from "@bitauth/libauth";
+
+const pubkey = new Uint8Array(
+  Buffer.from(
+    "f10d00bd9560d70898651d5f984e3b58b11d334efafee1ce9ce17afd36ecc3e2affc6446f0e25989fec1cc69abcdd02da1bc6b2744f265c99975beac4156daf2",
+    "hex"
+  )
+);
+const msgHash = new Uint8Array(
+  Buffer.from(
+    "cc8cbf34b5a24ad1562e0b6f9870a8d376aea8fc53d9fb3fc4485b41fbf464ec",
+    "hex"
+  )
+);
+const sig = new Uint8Array(
+  Buffer.from(
+    "92c3f7c6981fa678559ada4d179f91b19083ab8253cd7842dec969f2533fbb2282fe69799d7e59ed47f001ad08fcf2e38a7ccfe5a8957cf602882c9061860fe7",
+    "hex"
+  )
+);
+
+(async () => {
+  const secp256k1 = await instantiateSecp256k1();
+  secp256k1.verifySignatureDERLowS(sig, pubkey, msgHash)
+    ? console.log("🚀 Signature valid")
+    : console.log("❌ Signature invalid");
+})();
 
 function App() {
   return (
